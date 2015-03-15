a77dbeb5f00b7259ff74ef4a7519d3548a1194f0
Roy Klein <royk@commercesciences.com>
2015-03-15 07:41:01 +0200

    Work on search

--- app/controllers/project/edit.js
+++ app/controllers/project/edit.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
 	actions: {
 		save: function() {
+      var tags = this.get("model.description");
+      tags = tags.replace(/\s+/g, " ");
+      tags = tags.split(" ");
+      this.set("model.tags", tags);
 			this.get("model").save();
 		}
 	}
