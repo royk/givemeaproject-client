0f8a26bd34258ced5d15c1f3657bbae6dde8ed6e
Roy Klein <royk@commercesciences.com>
2015-03-31 18:16:44 +0300

    Work in progress

--- app/controllers/project/edit.js
+++ app/controllers/project/edit.js
@@ -1,12 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  platforms: function() {
+    return this.store.find("platform");
+  }.property(),
 	actions: {
 		save: function() {
       var tags = this.get("model.description");
-      tags = tags.replace(/\s+/g, " ");
-      tags = tags.split(" ");
-      this.set("model.tags", tags);
+      if (tags) {
+        tags = tags.replace(/\s+/g, " ");
+        tags = tags.split(" ");
+        this.set("model.tags", tags);
+      }
+      this.set("model.platform", this.get("platform.id"));
 			this.get("model").save();
 		}
 	}
--- app/models/project.js
+++ app/models/project.js
@@ -3,5 +3,6 @@ import DS from "ember-data";
 export default DS.Model.extend({
 	name: DS.attr("string"),
   description: DS.attr("string"),
-  tags: DS.attr()
+  tags: DS.attr(),
+  platform: DS.attr("string")
 });
